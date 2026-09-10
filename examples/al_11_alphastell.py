98b21d96cf52e2ed9b24d7898d2899d105712fe3
lzpel <18492524+lzpel@users.noreply.github.com>
2026-09-10 23:48:46 +0900

    feature/sweep

--- examples/al_11_alphastell.py
+++ examples/al_11_alphastell.py
@@ -1,6 +1,6 @@
 import math
 import pathlib
-from typing import Callable
+from typing import Callable, List, Tuple
 
 from alphastell import SurfaceFourierRZ, Geometry
 
@@ -12,21 +12,35 @@ def main(
 	with open(wout, "rb") as f:
 		surface = SurfaceFourierRZ.load(f)
 	out.parent.mkdir(parents=True, exist_ok=True)
-	write_step(torus(surface, lambda phi, theta: 0.0), out.with_suffix(".step"))
+	step_surface=torus(surface, make_surface=lambda phi, theta: 0.0)
+	write_step(step_surface, out.with_suffix(".surface.step"))
+	step_sweep=torus(surface, make_sweep=(
+		[[]]
+	))
+	write_step(step_sweep, out.with_suffix(".sweep.step"))
 
 
 def torus(
 	surface: SurfaceFourierRZ,
-	offset: Callable[[float, float], float],  # 磁気面法線に沿ったオフセット [m]。0 を返せば磁気面そのもの
+	make_surface: Callable[[float, float], float]|None=None,  # 磁気面法線に沿ったオフセット [m]。0 を返せば磁気面そのもの
+	make_sweep: Tuple[List[float], List[List[float]]]|None=None, # 最初の要素はxy xはtheta方向 yはs方向 
 	div_phi: int = 96,
 	div_theta: int = 40,
 	s: float = 1.0,  # LCFS (プラズマ最外縁)
 ) -> Geometry:
-	def point(phi: float, theta: float) -> list[float]:
-		p, n = surface.point_normal(phi, theta, s, False)
-		return [p[i] + n[i] * offset(phi, theta) for i in range(3)]
-	return Geometry.bspline_geometry([[point(math.tau * i / div_phi, math.tau * j / div_theta) for j in range(div_theta)] for i in range(div_phi)])
+	if make_surface:
+		def point(phi: float, theta: float) -> List[float]:
+			p, n = surface.point_normal(phi, theta, s, False)
+			return [p[i] + n[i] * make_surface(phi, theta) for i in range(3)]
+		return Geometry.bspline_geometry([[point(math.tau * i / div_phi, math.tau * j / div_theta) for j in range(div_theta)] for i in range(div_phi)])
+	if make_sweep:
+		height=0.4
+		width=0.5
+		profile = make_sweep[0]
 
+		paths = [[e for p in spine for e in guided(p)] for spine in projected_spines]
+		return Geometry.sweep_geometry(profile, paths)
+		
 
 def write_step(
 	geometry: Geometry,
